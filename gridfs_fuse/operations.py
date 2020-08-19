9b12d79704b0e83074b5ff3138358a27013f6b3c
José María Fernández <josemariafg@gmail.com>
2020-08-19 19:56:04 +0200

    Circumventing lack of time_ns method in python 3.6 and earliear

--- gridfs_fuse/operations.py
+++ gridfs_fuse/operations.py
@@ -24,6 +24,32 @@ def grid_in_size(grid_in):
     return grid_in._position + grid_in._buffer.tell()
 
 
+try:
+    time_ns = time.time_ns
+except:
+    import ctypes
+
+    CLOCK_REALTIME = 0
+
+    class timespec(ctypes.Structure):
+        _fields_ = [
+            ('tv_sec', ctypes.c_int64), # seconds, https://stackoverflow.com/q/471248/1672565
+            ('tv_nsec', ctypes.c_int64), # nanoseconds
+            ]
+
+    clock_gettime = ctypes.cdll.LoadLibrary('libc.so.6').clock_gettime
+    clock_gettime.argtypes = [ctypes.c_int64, ctypes.POINTER(timespec)]
+    clock_gettime.restype = ctypes.c_int64
+
+    def time_ns():
+        if hasattr(time.
+        tmp = timespec()
+        ret = clock_gettime(CLOCK_REALTIME, ctypes.pointer(tmp))
+        if bool(ret):
+            raise OSError()
+        return tmp.tv_sec * 10 ** 9 + tmp.tv_nsec
+
+
 class Entry(object):
     def __init__(self, ops, filename, inode, parent_inode, mode, uid, gid):
         self._ops = ops
@@ -35,7 +61,7 @@ class Entry(object):
         self.uid = uid
         self.gid = gid
 
-        self.atime = self.mtime = self.ctime = time.time_ns()
+        self.atime = self.mtime = self.ctime = time_ns()
 
         # Only for directories
         # filename: inode
